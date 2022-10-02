2b8329780d1212ee7ae9cdb3b59d81bae3cc5a4b
FahzainAhmad <41178886+FahzainAhmad@users.noreply.github.com>
2022-10-02 14:41:36 +0530

    Update Main.C

--- Main.C
+++ Main.C
@@ -1,37 +1,62 @@
+//IG : cybersoul.exe
+//Contact : saiyedfahzain191@gmail.com
+
+//Importing Required Libraries.
+
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-#include<dos.h>
 
 
 void main(){
+	
+	//Initializing required variables.
+	
+	//i is the iteration variable for loop.
+	//pos is the position of the character.
+	//reward is the position of the reward.
+	//score variable stores player's score.
+	//ctr stores player's movement commands.
+	
 	int i,pos,reward = rand() % 100 + 1, score = 0;
 	char ctr;
 	clrscr();
+	
+	//Initializing position as 0.
+	
 	pos = 0;
 	while(1){
 	clrscr();
+		
+		// Loop for rendering the Map.
+		
 		for(i=0;i<100;i++){
 			if(i%10==0){
-				printf("\n");
+				printf("\n"); // Next line after every 10 characters.
 			}
 			if(i==pos){
-				printf(" @");
+				printf(" @"); // Place the player.
 			}
 			else if(i==reward){
-				printf(" $");
+				printf(" $"); // Place the reward.
 			}
 			else{
-				printf(" *");
+				printf(" *"); //The moveable area.
 			}
 		}
-		printf("\n\nScore : %d", score);
+		
+		printf("\n\nScore : %d", score); // Show Score.
 		printf("\nEnter Your Commnd - WASD/ : ");
-		ctr = getchar();
-		if(pos == reward){
+		ctr = getchar(); // Save user command in ctr variable.
+		
+		// If Player and Reward are at the same position, Add score + 1 and generate Reward on another random position.
+		if(pos == reward){ 
 			score++;
 			reward = rand() % 100 + 1;
 		}
+		
+		// Move player according to command.
+		
 		if(ctr=='d'){
 			if(pos!=99){
 				 pos++;
@@ -64,6 +89,9 @@ void main(){
 				pos=pos;
 			}
 		}
+		
+		// Break the loop and exit if player enters "/".
+		
 		else if(ctr=='/'){
 			break;
 		}
